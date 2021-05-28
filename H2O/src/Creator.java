8eed3807d9909871d37b3928274cc9544b631d2f
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-05-28 16:43:28 +0300

    test

--- H2O/src/Creator.java
+++ H2O/src/Creator.java
@@ -7,23 +7,23 @@ import java.util.stream.StreamSupport;
 
 public class Creator {
     private final Map<String, Semaphore> semaphore;
-    private final CiclicBarrier barrier;
+    private final CyclicBarrier barrier;
     public Creator(Map<String, Integer> numberOfAtom) {
         semaphore = numberOfAtom.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, v -> new Semaphore(v.getValue())));
-        barrier = new CiclicBarrier(numberOfAtom.values().stream().mapToInt(i -> i).sum());
+        barrier = new CyclicBarrier(numberOfAtom.values().stream().mapToInt(i -> i).sum());
     }
 
     public void create(Iterable<String> atomList) {
         String finalString = StreamSupport.stream(atomList.spliterator(),false).collect(Collectors.joining());
-        System.out.println("Ввод: " + finalString + "\nВывод: ");
+        System.out.print("Ввод: " + finalString + "\nВывод: ");
         atomList.forEach(e -> new Thread(() -> atom(semaphore.get(e), e)).start());
     }
     @SneakyThrows
-    private void atom(Semaphore semaphore, String sym) {
-        semaphore.acquire();
+    private void atom(Semaphore semafore, String sym) {
+        semafore.acquire();
         barrier.await();
-        System.out.println(sym);
-        semaphore.release();
+        System.out.print(sym);
+        semafore.release();
     }
 }
